a14cde0b8f8cb8bb9d644fda9d78fcd228e7b744
Ahmed Abbas <111700915+Ahmed-Abbas-2077@users.noreply.github.com>
2023-09-03 02:15:16 +0300

    evaluate

--- script.js
+++ script.js
@@ -42,3 +42,88 @@ clear.addEventListener('click', () => {
   display.value = '';
   display.innerText = display.value;
 });
+
+equality.addEventListener('click', evaluate);
+
+function evaluate() {
+  const display = document.querySelector('.display');
+  const displayValue = display.value;
+  const displayArray = displayValue.split(' ');
+  if (displayArray.length % 2 === 0 || displayArray.length === 1) {
+    display.value = 'ERROR';
+    display.innerText = display.value;
+  } else {
+    for (let i = 0; i < displayArray.length; i++) {
+      if (
+        (displayArray[i] === '+' ||
+          displayArray[i] === '-' ||
+          displayArray[i] === '*' ||
+          displayArray[i] === '/') &&
+        (displayArray[i + 1] === '+' ||
+          displayArray[i + 1] === '-' ||
+          displayArray[i + 1] === '*' ||
+          displayArray[i + 1] === '/' ||
+          displayArray[i - 1] === '+' ||
+          displayArray[i - 1] === '-' ||
+          displayArray[i - 1] === '*' ||
+          displayArray[i - 1] === '/')
+      ) {
+        display.value = 'ERROR';
+        display.innerText = display.value;
+      }
+      if (displayArray[i] === '*') {
+        displayArray.splice(
+          i - 1,
+          3,
+          parseFloat(displayArray[i - 1]) * parseFloat(displayArray[i + 1])
+        );
+        i = 0;
+      } else if (displayArray[i] === '/') {
+        displayArray.splice(
+          i - 1,
+          3,
+          parseFloat(displayArray[i - 1]) / parseFloat(displayArray[i + 1])
+        );
+        i = 0;
+      }
+    }
+
+    for (let i = 0; i < displayArray.length; i++) {
+      if (
+        (displayArray[i] === '+' ||
+          displayArray[i] === '-' ||
+          displayArray[i] === '*' ||
+          displayArray[i] === '/') &&
+        (displayArray[i + 1] === '+' ||
+          displayArray[i + 1] === '-' ||
+          displayArray[i + 1] === '*' ||
+          displayArray[i + 1] === '/' ||
+          displayArray[i - 1] === '+' ||
+          displayArray[i - 1] === '-' ||
+          displayArray[i - 1] === '*' ||
+          displayArray[i - 1] === '/')
+      ) {
+        display.value = 'ERROR';
+        display.innerText = display.value;
+      }
+      if (displayArray[i] === '+') {
+        displayArray.splice(
+          i - 1,
+          3,
+          parseFloat(displayArray[i - 1]) + parseFloat(displayArray[i + 1])
+        );
+        i = 0;
+      } else if (displayArray[i] === '-') {
+        displayArray.splice(
+          i - 1,
+          3,
+          parseFloat(displayArray[i - 1]) - parseFloat(displayArray[i + 1])
+        );
+        i = 0;
+      }
+    }
+    display.value = displayArray[0];
+    display.innerText = display.value;
+    console.log(displayArray);
+  }
+}
